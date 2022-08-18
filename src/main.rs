27e4c20ee6b804e02c5171763d7ef02e1f45b42b
InterestingBrainPoops <satasiyahet@gmail.com>
2022-08-17 17:42:36 -0700

    seems to work, is slightly bugged

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,24 @@
 use std::collections::HashMap;
 
-use image::{DynamicImage, GenericImageView, Rgba};
+use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
 use rand::{distributions::WeightedIndex, prelude::Distribution, thread_rng};
 
 fn main() {
     println!("Hello, world!");
-    let img = image::open("origin.png").expect("File not found!");
+    let img = image::open("../origin.png").expect("File not found!");
 
     let mut map = HashMap::new();
     let mut inv_map = HashMap::new();
     let mut weights = HashMap::new();
-    let pairs = calc_pairs(img, &mut map, &mut weights, &mut inv_map);
+    let pairs = calc_pairs(img, &mut map, &mut inv_map, &mut weights);
     println!("{}", pairs.len());
     println!("{:?}", pairs);
 
-    let generated = generate(
-        &pairs,
-        img.width() as usize,
-        img.height() as usize,
-        &map,
-        &inv_map,
-        &weights,
-    );
+    let mut generated = generate(&pairs, 3, 1, &map, &inv_map, &weights);
+    while let Err(_) = generated {
+        generated = generate(&pairs, 31, 10, &map, &inv_map, &weights);
+    }
+    generated.expect("e").save("./output.png").unwrap();
 }
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
 enum Direction {
@@ -151,24 +148,32 @@ fn generate(
 ) -> Result<DynamicImage, String> {
     let mut rng = thread_rng();
     // all the waves
-    let mut waves = vec![vec![map.keys().collect::<Vec<&Rgba<u8>>>()]];
+    let mut waves = vec![vec![map.keys().collect::<Vec<&Rgba<u8>>>(); width]; height];
     // seed with a random square
     let mut seed = Some((0, 0));
     // go until all squares are all length of 1
     while waves.iter().flatten().any(|x| x.len() != 1) {
+        println!("{:#?}", waves);
         if waves.iter().flatten().any(|x| x.is_empty()) {
             return Err("Ran into a contradiction".to_string());
         }
+
         // pick the start square
         let start_square = match seed {
-            Some(square) => square,
-            None => {
+            Some(square) => {
                 seed = None;
+                square
+            }
+            None => {
                 // find the square with the smallest entropy
                 let mut square = (0, 0);
                 let mut smallest_entropy = 1000.0;
                 for (y, row) in waves.iter_mut().enumerate() {
                     for (x, wave) in row.iter_mut().enumerate() {
+                        if wave.len() == 1 {
+                            continue;
+                        }
+                        println!("{}", wave.len());
                         // calculate shannon entropy
                         let weight_sum: f64 =
                             wave.iter().map(|&x| *weights.get(x).unwrap() as f64).sum();
@@ -202,6 +207,7 @@ fn generate(
             .iter()
             .map(|&x| *weights.get(x).unwrap())
             .collect::<Vec<u64>>();
+        println!("dist : {:?}", dist);
         let ind = WeightedIndex::new(&dist).unwrap();
         waves[start_square.1][start_square.0] =
             vec![waves[start_square.1][start_square.0][ind.sample(&mut rng)]];
@@ -215,14 +221,13 @@ fn generate(
         for rule in triplets {
             if rule.original == *map.get(color).unwrap() {
                 match rule.orientation {
-                    Direction::Left => left_possibles.push(inv_map.get(&rule.next_to).unwrap()),
-                    Direction::Right => right_possibles.push(inv_map.get(&rule.next_to).unwrap()),
-                    Direction::Up => up_possibles.push(inv_map.get(&rule.next_to).unwrap()),
-                    Direction::Down => down_possibles.push(inv_map.get(&rule.next_to).unwrap()),
+                    Direction::Left => left_possibles.push(*inv_map.get(&rule.next_to).unwrap()),
+                    Direction::Right => right_possibles.push(*inv_map.get(&rule.next_to).unwrap()),
+                    Direction::Up => up_possibles.push(*inv_map.get(&rule.next_to).unwrap()),
+                    Direction::Down => down_possibles.push(*inv_map.get(&rule.next_to).unwrap()),
                 }
             }
         }
-
         let directions = vec![
             Direction::Up,
             Direction::Down,
@@ -244,7 +249,7 @@ fn generate(
                 }
                 Direction::Right => {
                     check_square = {
-                        if start_square.0 != width - 1 {
+                        if start_square.0 + 1 != width {
                             &mut waves[start_square.1][start_square.0 + 1]
                         } else {
                             continue;
@@ -262,7 +267,7 @@ fn generate(
                 }
                 Direction::Down => {
                     check_square = {
-                        if start_square.1 != height - 1 {
+                        if start_square.1 + 1 != height {
                             &mut waves[start_square.1 + 1][start_square.0]
                         } else {
                             continue;
@@ -287,7 +292,12 @@ fn generate(
         }
     }
 
-    let out = DynamicImage::new_rgb8(width as u32, height as u32);
+    let mut out = DynamicImage::new_rgb8(width as u32, height as u32);
+    for (y, row) in waves.iter().enumerate() {
+        for (x, wave) in row.iter().enumerate() {
+            out.put_pixel(x as u32, y as u32, *wave[0]);
+        }
+    }
 
     Ok(out)
 }
